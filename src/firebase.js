f5f52d7446ca8414238e1e7479a576a870ed203c
Cristian Lucero <cristianjlucero32@gmail.com>
2019-03-18 17:42:47 -0700

    Hid API Key

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 const config = {
-  apiKey: 'AIzaSyA-lKFmq2SlZjRfWG2nneYvVCuI84dznzk',
-  authDomain: 'commentform-1aca5.firebaseapp.com',
-  databaseURL: 'https://commentform-1aca5.firebaseio.com',
-  projectId: 'commentform-1aca5',
-  storageBucket: 'commentform-1aca5.appspot.com',
-  messagingSenderId: '580538636510'
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 firebase.initializeApp(config);
 
